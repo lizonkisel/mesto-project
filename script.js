bf3739a7ddaaab7cef4e260fcb087d757f27fd0a
lizonkisel <ees.hwork@gmail.com>
2021-10-19 13:03:13 +0300

    Variables for the first popup were changed. The second popup was added to JS

--- script.js
+++ script.js
@@ -3,21 +3,28 @@
   /* Открыть/закрыть поп-ап "Редактировать профиль" */
 
 let profileEdit = document.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.form__button-close');
+const popupEditProfile = document.querySelector('.popup_editProfile');
+const popupEditProfileCloseButton = popupEditProfile.querySelector('.form__button-close');
 
   /* Заполнение полей формы */
 
-let profileInformation = document.querySelector('.profile__profile-info');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-let popupName = document.querySelector('.form__item_type_name');
-let popupDescription = document.querySelector('.form__item_type_work');
+let popupEditProfileName = popupEditProfile.querySelector('.form__item_type_name');
+let popupEditProfileDescription = popupEditProfile.querySelector('.form__item_type_work');
 
   /* Изменение данных профиля */
 
-let popupForm = document.querySelector('.popup__form');
+let popupEditProfileForm = popupEditProfile.querySelector('.popup__form');
+
+  /* Добавление нового места */
+
+const popupNewPlace = document.querySelector('.popup_newPlace');
+const popupNewPlaceCloseButton = popupNewPlace.querySelector('.form__button-close');
+
+popupNewPlaceCloseButton.addEventListener('click', openPopup());
+
 
 
 /* ФУНКЦИИ */
@@ -25,28 +32,33 @@ let popupForm = document.querySelector('.popup__form');
   /* Открыть/закрыть поп-ап "Редактировать профиль" */
 
 function openPopup() {
-  popup.classList.add('popup_opened');
+  popupEditProfile.classList.add('popup_opened');
   changeProfileData();
 }
 
+/* function openPopup(popupName) {
+  popupName.classList.add('popup_opened');
+  changeProfileData();
+} */
+
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popupEditProfile.classList.remove('popup_opened');
 }
 
   /* Заполнение полей формы */
 
     /* Эту функцию, по-хорошему, надо переименовать */
 function changeProfileData() {
-  popupName.value = profileName.textContent;
-  popupDescription.value = profileDescription.textContent;
+  popupEditProfileName.value = profileName.textContent;
+  popupEditProfileDescription.value = profileDescription.textContent;
 }
 
   /* Изменение данных профиля */
 
 function formSubmit(form) {
   form.preventDefault();
-  profileName.textContent = popupName.value;
-  profileDescription.textContent = popupDescription.value;
+  profileName.textContent = popupEditProfileName.value;
+  profileDescription.textContent = popupEditProfileDescription.value;
 
   closePopup();
 }
@@ -55,5 +67,7 @@ function formSubmit(form) {
 /* ИСПОЛНЯЕМЫЙ КОД */
 
 profileEdit.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', formSubmit);
+popupEditProfileCloseButton.addEventListener('click', closePopup);
+popupEditProfileForm.addEventListener('submit', formSubmit);
+
+
